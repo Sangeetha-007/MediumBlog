7cd46eb5ab3bb9a8a650ec4a99ff5b371105b2eb
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2023-12-14 15:13:16 -0500

    Create countRegression.py

---
+++ countRegression.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+# Generate some example data
+np.random.seed(42)
+X = np.random.rand(100, 1) * 10  # Independent variable
+lambda_ = 2  # Poisson parameter
+y = np.random.poisson(lambda_, size=(100,))  # Dependent variable
+
+# Add some noise to make the data more realistic
+y += np.random.normal(0, 0.5, size=(100,))
+
+# Fit Poisson regression model
+X = sm.add_constant(X)  # Add a constant term for the intercept
+poisson_model = sm.Poisson(y, X).fit()
+
+# Predict using the model
+X_pred = np.linspace(0, 10, 100)
+X_pred = sm.add_constant(X_pred)
+y_pred = poisson_model.predict(X_pred)
+
+# Plot the data and regression line
+plt.scatter(X[:, 1], y, label='Actual Data', alpha=0.7)
+plt.plot(X_pred[:, 1], y_pred, label='Poisson Regression', color='red', linewidth=2)
+plt.xlabel('Independent Variable')
+plt.ylabel('Dependent Variable (Counts)')
+plt.title('Poisson Regression Example')
+plt.legend()
+plt.show()
